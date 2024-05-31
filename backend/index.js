3506c6b6119b46f019e128f8df3049d7afb88b2a
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-05-31 18:52:19 +0530

    Add middleware and routes to backend index.js

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,19 @@ const dotenv = require('dotenv');
 const app = express();
 dotenv.config();
 
+
+// Middleware
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL;
+
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 
 // Connect to MongoDB
