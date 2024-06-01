062ce88f6334d59c1222f34be9d9fa4787012f5e
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-01 08:32:36 +0530

    Refactor registerUser function in userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,46 @@
+const bcrypt = require("bcryptjs");
+
+const HTTPError = require("../models/errorModel.js");
+const User = require("../models/userModel.js");
+
+
 //Register a new user
-const registerUser = async(req, res) => {
-    res.json("Register a new user");
+const registerUser = async(req, res, next) => {
+    try {
+        const { name, email, password, password2 } = req.body;
+        if (!name || !email || !password) {
+            return next(new HTTPError("All fields are required." , 422));
+        }
+
+        const newEmail = email.toLowerCase();
+
+        //Check if user already exists
+        const emailExists = await User.findOne({ email: newEmail });
+        if (emailExists) {
+            return next(new HTTPError("Email already exists.", 422));
+        }
+
+        if (password.length < 8) {
+            return next(new HTTPError("Password should be at least 6 characters.", 422));
+        }
+
+        if (password !== password2) {
+            return next(new HTTPError("Passwords do not match.", 422));
+        }
+
+        //const salt = await bcrypt.genSalt(12);
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = await User.create({
+            name,
+            email: newEmail,
+            password: hashedPassword,
+        });
+
+        res.status(201).json(newUser.email + " has been registered successfully.");
+
+    } catch (error) {
+        return next(new HTTPError("Registration failed, please try again", 422));
+    }
 };
 
 
---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    picture: {
+        type: String,
+    },
+    posts: {
+        type: Number,
+        default: 0,
+    }
+});
+
+module.exports = model('User', userSchema);
