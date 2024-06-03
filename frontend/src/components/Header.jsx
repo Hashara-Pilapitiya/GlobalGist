cac338162012d627355e0f6f34247af8abdb1440
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-03 13:33:23 +0530

    Update npm dependencies and add react-icons package and code Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logo.png';
+import { FaBars } from "react-icons/fa";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+
+      <div className='container nav__container'>
+
+        <Link to='/' className='nav__logo'>
+          <img src={Logo} alt='logo' />
+        </Link>
+
+        <ul className='nav__menu'>
+          <li className='nav__menu-item'>
+            <Link to='/profile'>Hashara Pilapitiya</Link>
+          </li>
+          <li className='nav__menu-item'>
+            <Link to='/create'>Create a Post</Link>
+          </li>
+          <li className='nav__menu-item'>
+            <Link to='/authors'>Authors</Link>
+          </li>
+          <li className='nav__menu-item'>
+            <Link to='/logout'>Log Out</Link>
+          </li>
+        </ul>
+
+        <button className='nav__toggle-btn'>
+          <IoMdCloseCircle />
+        </button>
+
+      </div>
+
+    </nav>
   )
 }
 
