89798cb7ceecf8c24f971636b80bda4e213e849d
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-04 18:34:28 +0530

    Refactor CategoryPosts component to improve code readability

--- frontend/src/pages/CategoryPosts.jsx
+++ frontend/src/pages/CategoryPosts.jsx
@@ -9,11 +9,11 @@ const CategoryPosts = () => {
     return (
       
       <section className="category__posts" style={{marginTop: 130}}>
-      {posts.length > 0 ? <div className='container category__posts__container'>
-          {
-              posts.map(({id, thumbnail, category, title, desc, authorID}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} />)
-          }
-      </div> : <h2 className='center'>No Posts Found!</h2>}
+        {posts.length > 0 ? <div className='container category__posts__container'>
+            {
+                posts.map(({id, thumbnail, category, title, desc, authorID}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} />)
+            }
+        </div> : <h2 className='center'>No Posts Found!</h2>}
   </section>
 
   )
