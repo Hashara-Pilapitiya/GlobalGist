afc9518ffc63553809f22145a4a3e94345d27880
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-04 18:34:58 +0530

    Fix indentation in AuthorPosts component

--- frontend/src/pages/AuthorPosts.jsx
+++ frontend/src/pages/AuthorPosts.jsx
@@ -8,12 +8,12 @@ const AuthorPosts = () => {
 
   return (
     
-    <section className="author__posts" style={{marginTop: 130}}>
-    {posts.length > 0 ? <div className='container author__posts__container'>
-        {
-            posts.map(({id, thumbnail, category, title, desc, authorID}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} />)
-        }
-    </div> : <h2 className='center'>No Posts Found!</h2>}
+      <section className="author__posts" style={{marginTop: 130}}>
+      {posts.length > 0 ? <div className='container author__posts__container'>
+          {
+              posts.map(({id, thumbnail, category, title, desc, authorID}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} />)
+          }
+      </div> : <h2 className='center'>No Posts Found!</h2>}
 </section>
 
   )
