936e404d532fabe8b279b16c2f5db5165ba7e7ba
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-04 18:33:56 +0530

    Add CategoryPosts Page

--- frontend/src/pages/CategoryPosts.jsx
+++ frontend/src/pages/CategoryPosts.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DummyPosts } from '../data';
+import PostItem from '../components/PostItem';
 
 const CategoryPosts = () => {
-  return (
-    <div>CategoryPosts</div>
+    
+    const [posts, setPosts] = useState(DummyPosts); 
+
+    return (
+      
+      <section className="category__posts" style={{marginTop: 130}}>
+      {posts.length > 0 ? <div className='container category__posts__container'>
+          {
+              posts.map(({id, thumbnail, category, title, desc, authorID}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} />)
+          }
+      </div> : <h2 className='center'>No Posts Found!</h2>}
+  </section>
+
   )
 }
 
