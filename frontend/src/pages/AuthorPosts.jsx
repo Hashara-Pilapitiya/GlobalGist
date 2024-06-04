474c87a2868dca921091d96219c37f8592da40a1
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-04 18:27:49 +0530

    Add AuthorPosts Page

--- frontend/src/pages/AuthorPosts.jsx
+++ frontend/src/pages/AuthorPosts.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DummyPosts } from '../data';
+import PostItem from '../components/PostItem';
 
 const AuthorPosts = () => {
+
+  const [posts, setPosts] = useState(DummyPosts); 
+
   return (
-    <div>AuthorPosts</div>
+    
+    <section className="author__posts" style={{marginTop: 130}}>
+    {posts.length > 0 ? <div className='container author__posts__container'>
+        {
+            posts.map(({id, thumbnail, category, title, desc, authorID}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} />)
+        }
+    </div> : <h2 className='center'>No Posts Found!</h2>}
+</section>
+
   )
 }
 
