afe5e5e1510b1a7a7ed90aaa7e729db9053bf21b
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-04 11:59:22 +0530

    Add Error Page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdMoodBad } from "react-icons/md";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <section className='error__page'>
+      <div className='center'>
+        <Link to='/' className='btn primary'>Go Back Home</Link>
+        <h2>Page Not Found! <span><MdMoodBad /></span> </h2>
+      </div>
+    </section>
   )
 }
 
