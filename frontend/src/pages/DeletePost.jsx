dbd6bd82cb167936b86850e049409a6d8139e289
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-08 10:24:06 +0530

    Add user authentication to DeletePost page

--- frontend/src/pages/DeletePost.jsx
+++ frontend/src/pages/DeletePost.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { UserContext } from '../context/userContext';
 
 const DeletePost = () => {
+
+  const navigate = useNavigate();
+
+  const { currentUser } = useContext(UserContext);
+  const token = currentUser?.token;
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+    }
+  }, []);
+
   return (
     <div>DeletePost</div>
   )
