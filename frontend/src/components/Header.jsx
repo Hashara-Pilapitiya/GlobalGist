2586ba8ad59c0fa3331af848b203a0eb8fb2d6d2
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-08 10:04:10 +0530

    Add user authentication to navigation menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
-import React , { useState } from 'react';
+import React , { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/logo.png';
 import { FaBars } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 
+import { UserContext } from '../context/userContext';
+
 const Header = () => {
 
   const [showMenu, setShowMenu] = useState(window.innerWidth > 800 ? true : false);
+
+  const { currentUser } = useContext(UserContext);
 
   const closeNavHandler = () => {
     if(window.innerWidth < 800) {
@@ -25,7 +29,7 @@ const Header = () => {
           <img src={Logo} alt='logo' />
         </Link>
 
-        { showMenu && <ul className='nav__menu'>
+        { currentUser?.id && showMenu && <ul className='nav__menu'>
           <li className='nav__menu-item'>
             <Link to='/profile/fe5sdg' onClick={closeNavHandler}>Hashara Pilapitiya</Link>
           </li>
@@ -37,6 +41,15 @@ const Header = () => {
           </li>
           <li className='nav__menu-item'>
             <Link to='/logout'onClick={closeNavHandler}>Log Out</Link>
+          </li>
+        </ul> }
+
+        { !currentUser?.id && showMenu && <ul className='nav__menu'>
+          <li className='nav__menu-item'>
+            <Link to='/authors' onClick={closeNavHandler}>Authors</Link>
+          </li>
+          <li className='nav__menu-item'>
+            <Link to='/login'onClick={closeNavHandler}>Login</Link>
           </li>
         </ul> }
 
