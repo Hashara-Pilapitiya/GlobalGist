1d6e769400bd778a76663fc91d595350db73b04c
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-08 18:44:32 +0530

    Update CategoryPosts component to fetch posts by category from API

--- frontend/src/pages/CategoryPosts.jsx
+++ frontend/src/pages/CategoryPosts.jsx
@@ -1,20 +1,56 @@
-import React, { useState } from 'react';
-import { DummyPosts } from '../data';
+import React, { useState, useEffect } from 'react';
 import PostItem from '../components/PostItem';
+import Loader from '../components/Loader';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const CategoryPosts = () => {
     
-    const [posts, setPosts] = useState(DummyPosts); 
+  const [posts, setPosts] = useState([]);
 
-    return (
-      
-      <section className="category__posts" style={{marginTop: 130}}>
-        {posts.length > 0 ? <div className='container category__posts__container'>
-            {
-                posts.map(({id, thumbnail, category, title, desc, authorID}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} />)
-            }
-        </div> : <h2 className='center'>No Posts Found!</h2>}
+  const [isloading, setIsLoading] = useState(false);
+
+  const {category} = useParams();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await axios.get(`http://localhost:5000/api/posts/categories/${category}`);
+        
+        setPosts(response?.data)
+
+      } catch (error) {
+
+        console.log(error);
+
+      }
+
+      setIsLoading(false);
+
+    }
+
+    fetchPosts();
+
+  }, [])
+
+  if(isloading) {
+    return <Loader />
+  }
+
+  console.log(posts);
+
+return (
+  
+  <section className="posts" style={{marginTop: 130}}>
+      {posts.length > 0 ? <div className='container posts__container'>
+          {
+              posts.map(({_id: id, thumbnail, category, title, description, creator, createdAt}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={description} authorID={creator} createdAt={createdAt} />)
+          }
+      </div> : <h2 className='center'>No Posts Found!</h2>}
   </section>
+
 
   )
 }
